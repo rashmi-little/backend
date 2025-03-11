85fa35bb326d93aa63945a47ec32d439c6a0e105
Rashmi Ranjan Nayak <rrnayak165@gmail.com>
2025-03-11 10:56:47 +0530

    add : create user api and service interface method

--- src/main/java/com/mindfire/backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/mindfire/backend/service/impl/UserServiceImpl.java
@@ -1,4 +1,56 @@
 package com.mindfire.backend.service.impl;
 
-public class UserServiceImpl {
+import com.mindfire.backend.dto.request.UserRequestDto;
+import com.mindfire.backend.dto.response.UserResponseDto;
+import com.mindfire.backend.entity.User;
+import com.mindfire.backend.enums.Role;
+import com.mindfire.backend.repository.UserRepository;
+import com.mindfire.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final ModelMapper modelMapper;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserResponseDto create(UserRequestDto userRequestDto) {
+        User user = modelMapper.map(userRequestDto, User.class);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+
+        User savedUser = userRepository.save(user);
+
+        return modelMapper.map(savedUser, UserResponseDto.class);
+    }
+
+    @Override
+    public UserResponseDto update(long id, UserRequestDto userRequestDto) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(long id) {
+        return false;
+    }
+
+    @Override
+    public UserResponseDto getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<UserResponseDto> getAll() {
+        return List.of();
+    }
 }
