4b8328cb11b5b598ea5602b3454c5c928f3cf0bd
Lokanatham Latesh <lokanathamlatesh2003@gmail.com>
2025-03-11 13:02:33 +0530

    Basic JWT implementation completed.
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/com/mindfire/backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.mindfire.backend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+
+
+}
